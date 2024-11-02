10cf079c69424c6ad497153d40691c9503711f8b
Daniel <danielherrerahenao509@gmail.com>
2024-11-02 15:05:15 -0500

    "Correcciones tras la revisión de código"

--- src/main/java/Culturoteca/controllers/CultureMediaController.java
+++ src/main/java/Culturoteca/controllers/CultureMediaController.java
@@ -1,26 +1,20 @@
 package Culturoteca.controllers;
 
-import java.util.*;
+import java.util.List;
 
 import Culturoteca.exception.VideoNotFoundException;
 import Culturoteca.model.Video;
-import Culturoteca.service.impl.CultureMediaServiceImpl;
+import Service.impl.CultureMediaServiceImpl;
+import Service.CultureMediaService;
 
 public class CultureMediaController {
-
-    private final CultureMediaServiceImpl cultureMediaService;
-
+    private final CultureMediaService cultureMediaService;
 
     public CultureMediaController(CultureMediaServiceImpl cultureMediaService) {
         this.cultureMediaService = cultureMediaService;
     }
 
-
-    public List<Video> find_allVideos() throws VideoNotFoundException {
-        List<Video> videos = null;
-        videos = cultureMediaService.findAll();
-        return videos;
+    public List<Video> findAllVideos() throws VideoNotFoundException {
+        return cultureMediaService.findAll();
     }
-
-
 }
